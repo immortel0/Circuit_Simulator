4ce524939023b456d9dd2b82e9946219b476e064
immortel0 <immortalberryboi@gmail.com>
2025-11-01 14:02:15 +0530

    Revert "Added symbolic icons"
    This reverts commit f381c539a86aac28c8e726bb7904d1f0cb7e6a34.

--- component.py
+++ component.py
@@ -6,16 +6,14 @@ use component_list to map component names to indices
 
 """
 
+
+
 import tkinter as tk
-
-
-texture_pack = "Symbolic"
 
 class Resistor:
     def __init__(self, resistance = 1):
         self.resistance = resistance
-        self.image_symbolic = tk.PhotoImage(file = "Assets/texture_symbolic/resistor_symbol.png")
-        self.image_64x64 = tk.PhotoImage(file = "Assets/texture_64x64/resistor64x64.png")
+        self.image = tk.PhotoImage(file = "Assets/resistor64x64.png")
         self.voltage = 0
         self.current = 0
 
@@ -25,8 +23,7 @@ class Resistor:
 class Capacitor:
     def __init__(self, capacitance = 1):
         self.capacitance = capacitance
-        self.image_symbolic = tk.PhotoImage(file = "Assets/texture_symbolic/capacitor_symbol.png")
-        self.image_64x64 = tk.PhotoImage(file = "Assets/texture_64x64/capacitor64x64.png")
+        self.image = tk.PhotoImage(file = "Assets/capacitor64x64.png")
         self.voltage = 0
         self.current = 0
 
@@ -36,8 +33,7 @@ class Capacitor:
 class Inductor:
     def __init__(self, inductance = 1):
         self.inductance = inductance
-        self.image_symbolic = tk.PhotoImage(file = "Assets/texture_symbolic/inductor_symbol.png")
-        self.image_64x64 = tk.PhotoImage(file = "Assets/texture_64x64/inductor64x64.png")
+        self.image = tk.PhotoImage(file = "Assets/inductor64x64.png")
         self.voltage = 0
         self.current = 0
  
@@ -47,8 +43,7 @@ class Inductor:
 class Voltage_Source:
     def __init__(self, voltage = 1):
         self.voltage = voltage
-        self.image_symbolic = tk.PhotoImage(file = "Assets/texture_symbolic/voltage_symbol.png")
-        self.image_64x64 = tk.PhotoImage(file = "Assets/texture_64x64/voltage64x64.png")
+        self.image = tk.PhotoImage(file = "Assets/voltage64x64.png")
         self.current = 0   
 
     name = "Voltage Source"
@@ -66,10 +61,4 @@ component_list = {
     1: Capacitor,
     2: Inductor,
     3: Voltage_Source
-}
-
-
-texture_height = {
-    "Symbolic": 22,
-    "64x64": 64
 }
